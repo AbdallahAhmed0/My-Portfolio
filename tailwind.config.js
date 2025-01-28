dd460475945f86d70ba8c1a771716c72749fd6dc
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-01-28 18:48:17 +0200

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: { 
+      transitionDuration: {
+        300: '300ms',
+        500: '500ms',
+      },
+      scale: {
+        110: '1.1',
+      },
+      colors: {
+      header:'#75926e',
+      hero:'#5b7553',
+      accent: '#3e4f38',
+      background: '#F3F4F6',
+      text: '#374151',
+    },
   },
-  plugins: [],
+  },
+ plugins: [],
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: ['./src/**/*.{html,ts}'],
