aee406950a3407d1679b6157c478294cfdf06fe5
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-01-28 18:31:13 +0200

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.{html,ts}'],
+  },
+}
