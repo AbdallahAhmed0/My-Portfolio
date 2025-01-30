92a582a2d5ab89cd63120e868b2cb3350ebd8641
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-01-30 18:34:35 +0200

    edit style text of aboutme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { NgxParticlesModule } from "@tsparticles/angular";
+import { AboutComponent } from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
-  imports: [  NgxParticlesModule,HeaderComponent, HeroComponent ],
+  imports: [NgxParticlesModule, HeaderComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
