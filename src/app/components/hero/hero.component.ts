62ac88d041f705de77ee490def1f801e8e948189
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-01-30 17:20:30 +0200

    edit hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, AfterViewInit, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit,OnInit {
+
+
+
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+
+  ngOnInit(): void {
+  }
+   
+  ngAfterViewInit() {
+
+    if (isPlatformBrowser(this.platformId)) {
+      const texts = ['Abdallah Ahmed.', 'Front End Developer.'];
+      const animationElement = document.getElementById('animation-text');
+      let currentTextIndex = 0;
+      let currentCharIndex = 0;
+      let isDeleting = false;
+
+      const typeAndDelete = () => {
+        if (animationElement) {
+          if (isDeleting) {
+            animationElement.textContent = texts[currentTextIndex].slice(
+              0,
+              currentCharIndex--
+            );
+          } else {
+            animationElement.textContent = texts[currentTextIndex].slice(
+              0,
+              currentCharIndex++
+            );
+          }
+
+          if (!isDeleting && currentCharIndex === texts[currentTextIndex].length) {
+            isDeleting = true;
+            setTimeout(typeAndDelete, 1100); // Pause before deleting
+          } else if (isDeleting && currentCharIndex === 0) {
+            isDeleting = false;
+            currentTextIndex = (currentTextIndex + 1) % texts.length;
+            setTimeout(typeAndDelete, 700); // Pause before typing next text
+          } else {
+            setTimeout(typeAndDelete, isDeleting ? 150 : 200); // Typing/Deleting speed
+          }
+        }
+      };
+
+      typeAndDelete();
+    }
+  }
 }
+
