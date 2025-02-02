9b3b4174af52becbaf9b780ebb1a8fbb07a778ed
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-02-03 00:26:57 +0200

    connect my email in contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  sendEmail() {
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', 
+                     document.querySelector('form') as HTMLFormElement, 
+                     'YOUR_PUBLIC_KEY')
+      .then((result) => {
+        alert('Message sent successfully!');
+      })
+      .catch((error) => {
+        alert('Failed to send message. Please try again.');
+      });
+  }
 
 }
