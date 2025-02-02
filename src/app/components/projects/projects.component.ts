44237f2e2769cdce3211c86672ef187d3d6364a3
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-02-02 23:58:18 +0200

    add my projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -17,10 +17,25 @@ export class ProjectsComponent {
       live: 'https://my-store-angular.netlify.app'
     },
     {
-      name: 'Cine Glow',
-      description: 'A captivating movie website built with TypeScript, Next.js, and Tailwind CSS.',
-      image: 'assets/img/cine-glow.png',
-      link: 'https://your-cine-glow-link.com'
+      name: 'Examination Management System',
+      description: ' The project involved the design and implementation of an online examination platform, which enables the creation of exams, management of questions and the attempt of exams by students.',
+      image: 'img/img Projects/Exam.jpg',
+      link:'https://github.com/AbdallahAhmed0/Examination-Management-System',
+      live: 'https://drive.google.com/file/d/1IWtkho7F-cd3mPD_lauaRmJ5iKLIUbOH/view'
+    },
+    {
+      name: 'Quran Competition',
+      description: 'This project features a registration system for users to apply for Quran competitions. It also includes a dedicated site providing detailed information about the various Quran competitions available.',
+      image: 'img/img Projects/quran.jpg',
+      link:'https://github.com/AbdallahAhmed0/Registration-for-the-Quran-Competition',
+      live: 'https://quran-competition.netlify.app'
+    },
+    {
+      name: 'My Portfolio',
+      description: ' This project is a personal portfolio website that showcases my skills, projects, and contact information.',
+      image: 'img/img Projects/portfolio.jpg',
+      link:'https://github.com/AbdallahAhmed0/My-Portfolio',
+      live: 'https://drive.google.com/file/d/1IWtkho7F-cd3mPD_lauaRmJ5iKLIUbOH/view'
     }
   ];
 }
