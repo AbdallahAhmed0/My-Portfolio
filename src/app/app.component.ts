64fc5d1d50c2cc1ab4fc96f5d955582cd05acc8d
AbdallahAhmed <abdallahahmadadel@gmail.com>
2025-04-23 03:20:20 +0200

    add code of loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,33 @@ import { ProjectsComponent } from "./components/projects/projects.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import * as AOS from 'aos';
+import { LoadingComponent } from './components/loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NgxParticlesModule, HeaderComponent, HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
+  imports: [NgxParticlesModule, HeaderComponent, HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent,LoadingComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'portfolio';
+  isLoading = true;
+
+
   ngOnInit() {
+      
+    // Simulate loading delay
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 3500); // Change duration as needed
+    
     AOS.init({
       duration: 2000, // Animation duration
       once: false, // Whether animation should happen only once
       easing: 'ease-in-out', // Easing type
       offset: 100, // Offset (in pixels) from the original trigger point
-    });  }
+    }); 
+   }
 }
